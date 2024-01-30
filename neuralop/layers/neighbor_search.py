944a07535ba60e6f0714b011a2ecc0c11a24c189
cyberguli <cyberguli@cli-10-110-34-118.WIFIeduroamSTUD.units.it>
2024-01-30 08:42:29 -0500

    Reverted to torch cluster

--- neuralop/layers/neighbor_search.py
+++ neuralop/layers/neighbor_search.py
@@ -5,29 +5,26 @@ from torch import nn
 #Uses open3d by default which, as of 07/23/2023, requires torch 1.13.1
 class NeighborSearch(nn.Module):
     """Neighbor search within a ball of a given radius
-
     Parameters
     ----------
     use_open3d : bool
-        Whether to use open3d or torch_cluster
+        Wether to use open3d or torch_cluster
         NOTE: open3d implementation requires 3d data
     """
-    def __init__(self, use_open3d=True, use_torch_cluster=False):
+    def __init__(self, use_open3d=True):
         super().__init__()
-        if use_open3d: # slightly faster, works on GPU in 3d only
+        if use_open3d:
             from open3d.ml.torch.layers import FixedRadiusSearch
             self.search_fn = FixedRadiusSearch()
             self.use_open3d = use_open3d
-        else: # slower fallback, works on GPU and CPU
-            from .simple_neighbor_search import simple_neighbor_search
-            self.search_fn = simple_neighbor_search
+        else:
+            from torch_cluster import radius
+            self.search_fn = radius
             self.use_open3d = False
-        
-        
+
     def forward(self, data, queries, radius):
         """Find the neighbors, in data, of each point in queries
         within a ball of radius. Returns in CRS format.
-
         Parameters
         ----------
         data : torch.Tensor of shape [n, d]
@@ -62,6 +59,31 @@ class NeighborSearch(nn.Module):
             return_dict['neighbors_row_splits'] = search_return.neighbors_row_splits.long()
 
         else:
-            return_dict = self.search_fn(data, queries, radius)
-        
+            neighbors_count, neighbors_index = self.search_fn(data, queries, radius,
+                                               max_num_neighbors=data.shape[0])
+
+            if neighbors_count[-1] != queries.shape[0] - 1:
+                add_max_element = True
+                neighbors_count = torch.cat((neighbors_count,
+                                             torch.tensor([queries.shape[0] - 1], 
+                                                          dtype=neighbors_count.dtype,
+                                                          device=neighbors_count.device)), 
+                                                          dim=0)
+            else:
+                add_max_element = False
+
+            bins = torch.bincount(neighbors_count, minlength=1)
+            if add_max_element:
+                bins[-1] -= 1
+
+            neighbors_row_splits = torch.cumsum(bins, dim=0)
+            neighbors_row_splits = torch.cat((torch.tensor([0], 
+                                              dtype=neighbors_row_splits.dtype,
+                                              device=neighbors_row_splits.device),
+                                              neighbors_row_splits), dim=0)
+
+
+            return_dict['neighbors_index'] = neighbors_index.long()
+            return_dict['neighbors_row_splits'] = neighbors_row_splits.long()
+
         return return_dict
